04cd685e942570fdb36e9bc797f3324ce78fdf7f
Tim Gorsuch <gorsuchtim@gmail.com>
2016-03-16 21:33:52 -0500

    Create objects.js

---
+++ objects.js
@@ -0,0 +1,41 @@
+var page = [
+
+	{
+		name: "Photography Theme",
+		description: "html5/css3/bootstrap/jquery",
+		image: '../images/photographer.jpg',
+		location: 'photographyTheme/index.html',
+
+	},
+		{
+		name: "Bakery Theme",
+		description: "angularJS/html5/css3/bootstrap/jquery/javascript",
+		image: '../images/cupcake.jpg',
+		location: 'bakeryTheme/index.html',
+
+	},
+		{
+		name: "Form Validation",
+		description: "custom form validation with javascript",
+		image: '../images/validate.png',
+		location: 'jps/templates/form-validation.html',
+	},
+		{
+		name: "Image Caption",
+		description: "file upload &amp; manipulation with html5/css3/bootstrap/javascript",
+		image: '../images/caption.jpg',
+	},
+		{
+		name: "Javascript Challenges",
+		description: "solutions to various programming challenges",
+		image: '../images/js2.png',
+	},
+		{
+		name: "Math for Kids",
+		description: "javascript based math game",
+		image: '../images/math2.jpg',
+
+	},
+
+
+];
